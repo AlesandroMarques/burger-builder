69766571c7fe6a346cdb30b5c19327b8e90a4cdb
AlesandroMarques <alesandro.marques.am@gmail.com>
2020-09-10 15:38:10 -0400

    changed inine styles to outside if statement

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,20 +2,19 @@ import React from 'react';
 import modalCss from './Modal.module.css';
 
 //wrapper to display things , in this case wraps our order
-const modal = (props) => (
-<div className={modalCss.Modal}
-    style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1': ''
+const modal = (props) => {
+    let modalDisplay = null;
 
-    }}
+    if(props.show){
+        modalDisplay= (
+        <div className={modalCss.Modal}>
+            {props.children}
+        </div>);
 
->
-    {props.children}
-</div>
+    }
 
-
-
-);
+return(
+modalDisplay);
+};
 
 export default modal;
